bf89c6b120fd15de1f28b8aa4d366662f0a29b83
nian-hua <41846038+nian-hua@users.noreply.github.com>
2018-08-04 20:09:00 +0800

    Update ffkey.py

--- virginia/Examples/ffkey.py
+++ virginia/Examples/ffkey.py
@@ -1,3 +1,6 @@
+#转载请注明出处哦~~~~
+#https://github.com/nian-hua/
+
 import operator
 
 twoCipher = []
@@ -8,7 +11,7 @@ keylen = ""
 
 thiskey = ""
 
-fridnum = {
+fridnum = {                                                     #英文字母出现概率表
         'a':0.08167,'b':0.01492,'c':0.02782,'d':0.04253,
         'e':0.12702,'f':0.02228,'g':0.02015,'h':0.06094,
         'i':0.06966,'j':0.00153,'k':0.00772,'l':0.04025,
@@ -18,7 +21,7 @@ fridnum = {
         'y':0.01974,'z':0.00074,
         }
 
-binstr = {
+binstr = {                                                        #十六进制对照表
         '0':'0000',
         '1':'0001',
         '2':'0010',
@@ -39,7 +42,7 @@ binstr = {
 
 
 
-def Scanf():
+def Scanf():                                                        #输入函数
     
     global ciphertext,keylen
 
@@ -47,10 +50,10 @@ def Scanf():
 
     keylen = int(input("请输入密文长度:"))
         
-    ciphertext = ciphertext.lower()
+    ciphertext = ciphertext.lower()                                  #转换为小写字母
 
 
-def returntenum(nowstring):
+def returntenum(nowstring):                                          #将十六进制转换成十进制
 
     nowbin = ""
 
@@ -64,15 +67,14 @@ def returntenum(nowstring):
 
         nowten = nowten + int(nowbin[i])*(2**(7-i))
 
-    return(nowten)
+    return(nowten)                                                     #返回这个数的十进制
 
 
-
-def getkey(nowDic,nowlen):
+def getkey(nowDic,nowlen):                                             #这函数写的有点复杂，我自己都特么看不懂了。。尴尬
 
     print("------------------------------")
 
-    print(nowDic)
+    print(nowDic)                                                      #打印一下当前密文字典
 
     print("------------------------------")
     
@@ -80,16 +82,17 @@ def getkey(nowDic,nowlen):
 
     machtmp = []
 
-    for i in fridnum:
+    for i in fridnum:                                                  #根据密文长度，统计出明文应该出现的次数
 
         alphdic[i] = round(fridnum[i] * nowlen)
 
-    print(alphdic)
-
+    print(alphdic)                                                     #打印一下明文各字母应该出现的次数
+                                                                       #虽然每次循环都一样
     print("-----------------------------")
 
-
-    for i in range(3,int(nowlen*fridnum['e'])+1):
+    #    比较核心的for循环
+        
+    for i in range(3,int(nowlen*fridnum['e'])+1):                       #只循环字母出现次数在3~Max
 
         for  j in nowDic:
 
@@ -99,7 +102,7 @@ def getkey(nowDic,nowlen):
 
                     if (i-2)<=alphdic[k]<=(i+2):
 
-                       machtmp.append(returntenum(j) ^ ord(k))
+                       machtmp.append(returntenum(j) ^ ord(k))          #将猜测的密钥加入列表
 
     newmachdic = {}
 
@@ -111,7 +114,7 @@ def getkey(nowDic,nowlen):
 
         newmachdic[i] += 1 
 
-    newmachdic_t = sorted(newmachdic.items(),key=operator.itemgetter(1),reverse = True)
+    newmachdic_t = sorted(newmachdic.items(),key=operator.itemgetter(1),reverse = True)   #重新统计
 
     print("因子:次数")
 
@@ -128,49 +131,43 @@ def getkey(nowDic,nowlen):
             break
 
 
-
-
-
-
-
-
-
-def FreqAnaly(nowList): #频率分析
+def FreqAnaly(nowList):                                                 #频率分析
     
     nowDic = {}
 
-    for i in nowList:
+    for i in nowList:                                                   #初始化nowDic字典
         
         nowDic[i] = 0
 
-    for i in nowList:
+    for i in nowList:                                                   #统计某密文出现的次数
 
         nowDic[i] += 1
     
    # print(nowDic)
     
-    for i in nowDic:
+    for i in nowDic:                                                    #打印出统计频率look look
 
         print("%s:%s"%(i,nowDic[i]*'█'))
 
-    getkey(nowDic,len(nowList))
+    getkey(nowDic,len(nowList))                                         #猜解密钥函数
 
 
-def madeToTwo():
+def madeToTwo():                                                #因为这是HEX表示的，所以转成十进制的时候需要两个字符
 
     global twoCipher
  
     for  i in range(len(ciphertext)//2):
         
-        twoCipher.append(ciphertext[i*2:i*2+2])
+        twoCipher.append(ciphertext[i*2:i*2+2])                 #放到了twoCipher数组里
 
     print(twoCipher)
+
 
 def StrSplit():
 
     for i in range(keylen):
 
-        FreqAnaly(twoCipher[i::keylen])
+        FreqAnaly(twoCipher[i::keylen])                         #使用FreqAnaly函数处理每个密文分量
 
         print("----------")
 
@@ -182,9 +179,6 @@ def main():
     madeToTwo()
 
     StrSplit()
-
-
-
 
 
 if "__main__" == __name__:
