c5bf7b232bc388a8ad590d2914b4be3cbdc57f29
nian-hua <41846038+nian-hua@users.noreply.github.com>
2018-08-25 11:00:14 +0800

    Update uncode.py

--- caesar/uncode.py
+++ caesar/uncode.py
@@ -1,10 +1,20 @@
+###
+###    Date:2018-08-25
+###   Time:09:00 GMT
+###  Author:nianhua
+###
 
+from ngram_score import *
+
+fitness = ngram_score('quadgrams.txt')
 alphabetb = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 alphabets = "abcdefghijklmnopqrstuvwxyz"
-
+score = []
+goodscore = -99e9
+goodvi = 0
 ciphertext = input("请输入要破解的密码:")
 
-for i in range(1,26):
+def caser(i):
 
     plaintext =""
 
@@ -22,4 +32,24 @@ for i in range(1,26):
 
             plaintext += j
     
+    return plaintext
+
+
+
+for i in range(1,26):
+
+    plaintext = caser(i) 
+    
+    score.append(fitness.score(''.join(list(filter(str.isalpha,plaintext))).upper()))
+    
     print("%2d:%s"%(i,plaintext))
+
+for i in range(25):
+
+    if score[i] > goodscore :
+
+        goodscore = score[i]
+
+        goodvi = i+1
+
+print("最好的明文:%s"%caser(goodvi))
